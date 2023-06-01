3112207cba65cb837ddd5bc0409e4284988f4797
Keyang Xu <keyang.xu@petuum.com>
2023-06-01 19:23:02 -0400

    update description and examples

--- autoagents/utils/constants.py
+++ autoagents/utils/constants.py
@@ -1,13 +1,15 @@
 MAIN_HEADER = "Web Search Agent"
 
 MAIN_CAPTION = """This is a proof-of-concept search agent that reasons, plans, and executes web searches to collect information on your behalf.
-It will try to dissect your task and resolve it step by step.
+It aims to resolve your question by breaking it down into step-by-step subtasks.
 All the intermediate results will be presented."""
 
 SAMPLE_QUESTIONS = [
-    "Recommend a kid friendly movie that is playing at a theatre near sunnyvale. Give me the showtimes and a link to purchase the tickets.",
     "Who is the most recent NBA MVP? Which team does he play for? What are his career stats?",
-    "Who is the head coach of AC Milan now? How long has he been coaching the team?",
+    # "Who is the head coach of AC Milan now? How long has he been coaching the team?",
+    "Recommend a kid friendly movie that is playing at a theatre near sunnyvale. Give me the showtimes and a link to purchase the tickets.",
     "What is the mortgage rate right now and how does that compare to the past two years?",
     "What is the weather like in San Francisco today? What about tomorrow?",
+    "When and where is the upcoming concert for Taylor Swift? Share a link to purchase tickets.",
+    "Find me any nodalization studies that use SAS4A/SASSYS-1. Provide the links to each study found."
 ]
