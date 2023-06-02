622b83b24108fc76ab168b8e443a38e05b065041
Keyang Xu <keyang.xu@petuum.com>
2023-06-02 13:47:33 -0400

    Switch exmaple queries

--- autoagents/utils/constants.py
+++ autoagents/utils/constants.py
@@ -5,11 +5,12 @@ It aims to resolve your question by breaking it down into step-by-step subtasks.
 All the intermediate results will be presented."""
 
 SAMPLE_QUESTIONS = [
+    "Recommend me a movie in theater now to watch with kids.",
     "Who is the most recent NBA MVP? Which team does he play for? What are his career stats?",
-    # "Who is the head coach of AC Milan now? How long has he been coaching the team?",
-    "Recommend a kid friendly movie that is playing at a theatre near sunnyvale. Give me the showtimes and a link to purchase the tickets.",
+    "Who is the head coach of AC Milan now? How long has he been coaching the team?",
     "What is the mortgage rate right now and how does that compare to the past two years?",
     "What is the weather like in San Francisco today? What about tomorrow?",
     "When and where is the upcoming concert for Taylor Swift? Share a link to purchase tickets.",
-    "Find me any nodalization studies that use SAS4A/SASSYS-1. Provide the links to each study found."
+    "Find me recent studies focusing on hallucination in large language models. Provide the link to each study found.",
+    # "Find me any nodalization studies that use SAS4A/SASSYS-1. Provide the links to each study found."
 ]
