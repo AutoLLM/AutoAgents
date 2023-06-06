ee18882765eec7f083d8ffc70f413959fcea4906
Omkar Pangarkar <omkarenator@gmail.com>
2023-06-06 13:57:18 -0400

    fix spaces readme and disclaimer

--- autoagents/utils/constants.py
+++ autoagents/utils/constants.py
@@ -3,10 +3,12 @@ MAIN_HEADER = "Web Search Agent"
 MAIN_CAPTION = """This is a proof-of-concept search agent that reasons, plans,
 and executes web searches to collect information on your behalf.  It aims to
 resolve your question by breaking it down into step-by-step subtasks. All the
-intermediate results will be presented. DISCLAIMER: We are collecting search
-queries, so please refrain from providing any personal information. If you wish
-to avoid this, you can run the app locally by following the instructions on our
-Github."""
+intermediate results will be presented.
+
+*DISCLAIMER*: We are collecting search queries, so please refrain from
+providing any personal information. If you wish to avoid this, you can run the
+app locally by following the instructions on our
+[Github](https://github.com/AutoLLM/AutoAgents)."""
 
 SAMPLE_QUESTIONS = [
     "Recommend me a movie in theater now to watch with kids.",
@@ -16,5 +18,4 @@ SAMPLE_QUESTIONS = [
     "What is the weather like in San Francisco today? What about tomorrow?",
     "When and where is the upcoming concert for Taylor Swift? Share a link to purchase tickets.",
     "Find me recent studies focusing on hallucination in large language models. Provide the link to each study found.",
-    # "Find me any nodalization studies that use SAS4A/SASSYS-1. Provide the links to each study found."
 ]
