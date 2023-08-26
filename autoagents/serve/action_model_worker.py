14f4c6ce99bc5fe8456934fb5afbfa64369ad1d4
Omkar Pangarkar <omkarenator@gmail.com>
2023-08-26 13:02:07 -0400

    add action model_worker

---
+++ autoagents/serve/action_model_worker.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastchat.conversation import (
+        SeparatorStyle,
+        Conversation,
+        register_conv_template
+)
+
+import fastchat.serve.model_worker as model_worker
+
+if __name__ == "__main__":
+    # Action LLM default template
+    register_conv_template(
+        Conversation(
+            name="action",
+            system_message="Below is a goal you need to achieve. Given the available tools and history of past actions provide the next action to perform.",
+            roles=("### Goal", "### Tools", "### History", "### Next action"),
+            messages=(),
+            offset=0,
+            sep_style=SeparatorStyle.ADD_COLON_SINGLE,
+            sep="\n\n",  # separator between roles
+            )
+    )
+    args, model_worker.worker = model_worker.create_model_worker()
+    uvicorn.run(model_worker.app, host=args.host, port=args.port, log_level="info")
