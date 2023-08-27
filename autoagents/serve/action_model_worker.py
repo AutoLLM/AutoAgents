033df254a7d648142b2022e57f3fd90febc4dcbd
Omkar Pangarkar <omkarenator@gmail.com>
2023-08-27 00:37:32 -0400

    register ActionAdapter

--- autoagents/serve/action_model_worker.py
+++ autoagents/serve/action_model_worker.py
@@ -1,11 +1,44 @@
 import uvicorn
+import fastchat.serve.model_worker as model_worker
 from fastchat.conversation import (
         SeparatorStyle,
         Conversation,
-        register_conv_template
+        register_conv_template,
+        get_conv_template
+)
+from fastchat.model.model_adapter import (
+        register_model_adapter,
+        BaseModelAdapter
+)
+from transformers import (
+    AutoConfig,
+    AutoModelForCausalLM,
+    AutoTokenizer
 )
 
-import fastchat.serve.model_worker as model_worker
+class ActionAdapter(BaseModelAdapter):
+    """The model adapter for Action Vicuna"""
+
+    def match(self, model_path: str):
+        return "action" in model_path
+
+    def load_model(self, model_path: str, from_pretrained_kwargs: dict):
+        config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_path, config=config, trust_remote_code=True
+        )
+        model = AutoModelForCausalLM.from_pretrained(
+            model_path,
+            config=config,
+            trust_remote_code=True,
+            low_cpu_mem_usage=True,
+            **from_pretrained_kwargs,
+        )
+        return model, tokenizer
+
+    def get_default_conv_template(self, model_path: str) -> Conversation:
+        return get_conv_template("action")
+
 
 if __name__ == "__main__":
     # Action LLM default template
@@ -20,6 +53,7 @@ if __name__ == "__main__":
             sep="\n\n",  # separator between roles
             )
     )
+    register_model_adapter(ActionAdapter)
     args, model_worker.worker = model_worker.create_model_worker()
     # hardcode the conv template
     args.conv_template = "action"
