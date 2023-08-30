61db9045956f5ae4f4c2b7d29f724bcb7c53c9b1
yuqi.wang <yuqi.wang@petuum.com>
2023-08-30 14:31:16 -0400

    support prompt v3

--- autoagents/agents/agents/search.py
+++ autoagents/agents/agents/search.py
@@ -134,7 +134,6 @@ class CustomPromptTemplate(StringPromptTemplate):
     # The list of tools available
     tools: List[Tool]
     ialogger: InteractionsLogger
-    search_tool_name: str = "Search"
 
     def format(self, **kwargs) -> str:
         # Get the intermediate steps [(AgentAction, Observation)]
@@ -169,7 +168,6 @@ class CustomOutputParser(AgentOutputParser):
     llm: BaseLanguageModel
     new_action_input: Optional[str]
     action_history = defaultdict(set)
-    search_tool_name: str = "Search"
 
     def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
         parsed = json.loads(llm_output)
@@ -208,8 +206,7 @@ class ActionRunner:
                  llm: BaseLanguageModel,
                  persist_logs: bool = False,
                  prompt_template: str = template,
-                 tools: List[Tool] = [search_tool, note_tool, finish_tool],
-                 search_tool_name: str = "Search"):
+                 tools: List[Tool] = [search_tool, note_tool, finish_tool]):
         self.ialogger = InteractionsLogger(name=f"{uuid.uuid4().hex[:6]}", persist=persist_logs)
         tools = [search_tool, note_tool, finish_tool]
         prompt = CustomPromptTemplate(template=template,
@@ -217,7 +214,7 @@ class ActionRunner:
                                       input_variables=["input", "intermediate_steps"],
                                       ialogger=self.ialogger)
 
-        output_parser = CustomOutputParser(ialogger=self.ialogger, llm=llm, search_tool_name=search_tool_name)
+        output_parser = CustomOutputParser(ialogger=self.ialogger, llm=llm)
         self.model_name = llm.model_name
 
         class MyCustomHandler(AsyncCallbackHandler):
