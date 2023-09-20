f0a1847f0bcd6b71fc81b8813874ae0eb2d52da4
Suqi Sun <suqi.sun@petuum.com>
2023-09-20 16:21:20 +0000

    refactor metrics.py to integrate more metrics

--- autoagents/eval/metrics.py
+++ autoagents/eval/metrics.py
@@ -1,55 +1,204 @@
+import os
 import json
+import glob
+from argparse import ArgumentParser
+from collections import Counter, defaultdict
+import numpy as np
+from pprint import pprint
 
 
 def get_common_stats(log_files):
     stats = {
-            "average_search_invoked": 0,
-            "average_notepad_invoked": 0,
-            "average_rewritten": 0,
-            "invalid_tools_error": 0,
-            "average_steps": 0,
-            "parse_error": 0,
-            "context_len_error": 0,
-            "total_samples": 0,
-            "average_answer_missing": 0,
-            "finished_samples": 0,
-            "dns_error": 0
+        "average_search_invoked": 0,
+        "average_notepad_invoked": 0,
+        "average_rewritten": 0,
+        "invalid_tools_error": 0,
+        "average_steps": 0,
+        "parse_error": 0,
+        "context_len_error": 0,
+        "total_samples": 0,
+        "average_answer_missing": 0,
+        "finished_samples": 0,
+        "dns_error": 0,
+        "counts": Counter(),  # general counters
+        "plan_counts": Counter(),  # plan patterns
+        "len_history_trace": [],
+        "len_initial_plan": []
     }
+    metrics = Counter()
     samples = set()
     finished_samples = set()
     for file in log_files:
         with open(file, "r") as f:
             log_data = json.load(f)
-            has_error = any([True if "error" in entry else False for entry in log_data])
-            for entry in log_data:
-                if "goal" in entry:
-                    question = entry["goal"]
-                    samples.add(question)
-                if has_error:
-                    if "error" in entry:
-                        if "Could not parse LLM output:" in entry["error"]:
-                            stats["parse_error"] += 1
-                        elif "Invalid tool requested by the model." in entry["error"]:
-                            stats["invalid_tools_error"] += 1
-                        elif "This model's maximum context length is" in entry["error"]:
-                            stats["context_len_error"] += 1
-                else:
-                    if "query_rewrite" in entry:
-                        stats["average_rewritten"] += 1
-                    if "conversations" in entry:
-                        stats["average_steps"] += 1
-                        prediction = json.loads(entry["conversations"][-1]["value"])
-                        action = prediction["action"]
-                        if action == "Tool_Search" or action == "Tool_Wikipedia":
-                            stats["average_search_invoked"] += 1
-                        elif action == "Tool_Notepad":
-                            stats["average_notepad_invoked"] += 1
-                        elif action == "Tool_Finish":
-                            finished_samples.add(question)
+            summary = get_summary_from_log_data(log_data=log_data)
+            metrics += summary["stats"]
+            stats["counts"] += summary["counts"]
+            stats["plan_counts"] += summary["plan_counts"]
+            stats["len_history_trace"].extend(summary["len_history_trace"])
+            stats["len_initial_plan"].extend(summary["len_initial_plan"])
+            if summary["question"] is not None:
+                samples.add(summary["question"])
+                if summary["answer"] is not None:
+                    finished_samples.add(summary["question"])
+    stats.update(metrics)
     stats["total_samples"] = len(samples)
     stats["finished_samples"] = len(finished_samples)
     stats["average_steps"] = stats["average_steps"] / len(log_files)
     stats["average_rewritten"] = stats["average_rewritten"] / len(log_files)
     stats["average_search_invoked"] = stats["average_search_invoked"] / len(log_files)
     stats["average_notepad_invoked"] = stats["average_notepad_invoked"] / len(log_files)
+
+    hist, rng = np.histogram(stats["len_history_trace"], bins=range(0, 16))
+    stats["len_history_trace"] = hist.tolist()
+    
+    hist, rng = np.histogram(stats["len_initial_plan"], bins=range(0, 16))
+    stats["len_initial_plan"] = hist.tolist()
+
     return stats
+
+
+def get_summary_from_log_data(log_data: list):
+
+    stats = Counter()
+    question = answer = None
+
+    has_error = any([True if "error" in entry else False for entry in log_data])
+    for entry in log_data:
+        if "goal" in entry:
+            question = entry["goal"]
+        if has_error:
+            if "error" in entry:
+                if "Could not parse LLM output:" in entry["error"]:
+                    stats["parse_error"] += 1
+                elif "Invalid tool requested by the model." in entry["error"]:
+                    stats["invalid_tools_error"] += 1
+                elif "This model's maximum context length is" in entry["error"]:
+                    stats["context_len_error"] += 1
+        else:
+            if "query_rewrite" in entry:
+                stats["average_rewritten"] += 1
+            if "conversations" in entry:
+                stats["average_steps"] += 1
+                prediction = json.loads(entry["conversations"][-1]["value"])
+                action = prediction["action"]
+                if action == "Tool_Search" or action == "Tool_Wikipedia":
+                    stats["average_search_invoked"] += 1
+                elif action == "Tool_Notepad":
+                    stats["average_notepad_invoked"] += 1
+                elif action == "Tool_Finish":
+                    answer = prediction["action_input"]
+    return dict(
+        stats=stats,
+        question=question,
+        answer=answer,
+        **get_counters_from_log_data(log_data=log_data)
+    )
+
+
+def get_counters_from_log_data(log_data: list):
+
+    counts = Counter()
+    plan_counts = Counter()
+    LEN_INIT_PLAN = []
+    LEN_HIST_TRACE = []
+
+    counters = dict(
+        counts=counts,
+        plan_counts=plan_counts,
+        len_history_trace=LEN_HIST_TRACE,
+        len_initial_plan=LEN_INIT_PLAN
+    )
+
+    for item in log_data:
+        if "id" in item:
+            counts["conv_len"] += 1
+    counts["total"] += 1
+
+    for d in log_data:
+        if "error" in d:
+            if "Expecting value" in d["error"]:
+                # This is the old rewrite error
+                pass
+            elif "This model's maximum context length" in d["error"]:
+                counts["out_of_context"] += 1
+                if len(log_data) < 4:
+                    return counters
+            elif "[Errno -3] Temporary failure in name resolution" in d["error"]:
+                counts["dns_error"] += 1
+            else:
+                counts["parsing_errors"] += 1
+                return counters
+        elif "query_rewrite" in d:
+            counts["query_rewrite"] += 1
+            return counters
+    counts["total_valid"] += 1
+
+    # do last-step history analysis, log_data[-3]
+    last_convo = log_data[-3]["conversations"]
+    if last_convo[1]["from"] in ("gpt", "ai"):
+        # we don't have the system key in prompt_v3
+        output = json.loads(last_convo[1]["value"])
+    else:
+        output = json.loads(last_convo[2]["value"])
+
+    counts[f"EndWith_{output['action']}"] += 1
+
+    if last_convo[0]["from"] == "history":
+        hist = last_convo[0]["value"]
+        actions = [h["action"] for h in hist]
+        if len(actions) < 5 and len(actions) > 0:
+            actions_str = "->".join(actions) 
+            plan_counts[actions_str] += 1
+            if actions_str == "Tool_Notepad":
+                pass
+            if actions_str == "Tool_Search->Tool_Search->Tool_Notepad":
+                pass
+            if actions_str == "Tool_Search->Tool_Search->Tool_Search->Tool_Notepad":
+                pass
+        plans = []
+        for plan in [h["plan"] for h in hist]:
+            plans.extend(plan)
+        for plan in plans:
+            if plan.startswith("Visit"):
+                counts["visit_in_plan"] += 1
+                break
+
+        len_hist = len(hist)
+        if len_hist > 0:
+            len_plan0 = len(hist[0]["plan"])
+            LEN_INIT_PLAN.append(len_plan0)
+            if len_plan0 == 1:
+                pass
+        counts["len_hist"] += len_hist
+        LEN_HIST_TRACE.append(len_hist)
+
+        # find out if there are duplicate action+action_inputs
+        inputs = defaultdict(set)
+        plans = set()
+        for h in hist + [output]:
+            if h["action"] in inputs:
+                if h["action_input"] in inputs[h["action"]]:
+                    if output["action"] == "Tool_Finish":
+                        counts["Finish_with_dups"] += 1
+                        break
+                    else: # only count duplicates that didn't finish
+                        counts["duplicate_actions"] += 1
+                        break
+            inputs[h["action"]].add(h["action_input"])
+
+    return counters
+
+
+def main():
+
+    parser = ArgumentParser()
+    parser.add_argument("log_dir", type=str, help="path of the log directory")
+    args = parser.parse_args()
+
+    stats = get_common_stats(log_files=glob.glob(f"{args.log_dir}/*.json"))
+    pprint(stats)
+
+
+if __name__ == "__main__":
+    main()
